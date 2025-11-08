8891e7ee82fba09b730639deb0cdf3374aab3ba2
Sungwoo <sungwoo@example.com>
2025-11-08 18:09:42 +0900

    Add initial Unreal Engine Project

---
+++ Source/InteractionPractice/InteractionPractice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InteractionPractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InteractionPractice, "InteractionPractice" );
