a0fd6b9bf6ae28b15222232ed1c5e53f40249464
Sungwoo <sungwoo@example.com>
2025-11-08 22:34:40 +0900

    CheckDir 정면/후면 체크 기능 활성화 및 3인칭 템플릿 추가

---
+++ Source/InteractionPractice/Door/DoorBase.cpp
@@ -0,0 +1,87 @@
+﻿#include "DoorBase.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+ADoorBase::ADoorBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Frame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Frame"));
+	SetRootComponent(Frame);
+	
+	Panel = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Panel"));
+	Panel->SetupAttachment(Frame);
+	Panel->SetRelativeLocation(FVector(-55.f, 0.f, 3.f));
+	
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	Arrow->SetupAttachment(Frame);
+	Arrow->SetRelativeRotation(FRotator(0.f, 90.f, 0.f));
+	Arrow->SetRelativeLocation(FVector(0.f, 0.f, 130.f));
+	
+	OverlapBox = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapBox"));
+	OverlapBox->SetupAttachment(Frame);
+	OverlapBox->SetRelativeScale3D(FVector(2.f, 5.f, 4.f));
+	OverlapBox->SetRelativeLocation(FVector(0.f, 0.f, 130.f));
+	
+	OverlapBox->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OverlapEvent);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> DoorFrame(TEXT("/Script/Engine.StaticMesh'/Game/Mesh/Door/Door_frame.Door_frame'"));
+	if (DoorFrame.Succeeded())
+	{
+		Frame->SetStaticMesh(DoorFrame.Object);
+	}
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> DoorPanel(TEXT("/Script/Engine.StaticMesh'/Game/Mesh/Door/Door_panel.Door_panel'"));
+	if (DoorPanel.Succeeded())
+	{
+		Panel->SetStaticMesh(DoorPanel.Object);
+	}
+}
+
+void ADoorBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ADoorBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ADoorBase::OverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor == UGameplayStatics::GetPlayerCharacter(GetWorld(),0))
+	{
+		CheckDir();
+	}
+}
+
+void ADoorBase::CheckDir()
+{
+	FVector ArrowForwardVector = Arrow->GetForwardVector();
+	FVector ArrowWorldLocation = Arrow->GetComponentLocation();
+	FVector CharacterLocation = UGameplayStatics::GetPlayerCharacter(GetWorld(),0)->GetActorLocation();
+	FVector BetweenLength = ArrowWorldLocation - CharacterLocation;
+	
+	FVector NormalizeLength = BetweenLength.GetSafeNormal();
+	
+
+	float InnerProduct = ArrowForwardVector.Dot(NormalizeLength);
+
+	if (InnerProduct > 0)
+	{
+		bCorrectDirection = true;
+		UE_LOG(LogTemp, Warning, TEXT("후면"));
+	}
+	else
+	{
+		bCorrectDirection = false;
+		UE_LOG(LogTemp, Warning, TEXT("정면"));
+	}
+}
+
---
+++ Source/InteractionPractice/Door/DoorBase.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include "GameFramework/Actor.h"
+#include "DoorBase.generated.h"
+
+class UArrowComponent;
+class UBoxComponent;
+
+UCLASS()
+class INTERACTIONPRACTICE_API ADoorBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADoorBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// 문 프레임
+	UPROPERTY(BlueprintReadWrite,EditDefaultsOnly,  meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent>	Frame;
+
+	// 문 짝
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent>	Panel;
+
+	// 화살표
+	UPROPERTY(BlueprintReadWrite,EditDefaultsOnly,  meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UArrowComponent>			Arrow;
+
+	// 콜리전박스
+	UPROPERTY(BlueprintReadWrite,EditDefaultsOnly , meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent>		OverlapBox;
+
+	// 방향 (정면/후면)
+	bool bCorrectDirection;
+
+	UFUNCTION()
+	void OverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+protected:
+	void CheckDir();
+
+public:
+	bool GetCorrectDirection() const
+	{
+		return bCorrectDirection;
+	}
+	
+	void SetCorrectDirection(bool Dir)
+	{
+		bCorrectDirection = Dir;
+	}
+};
