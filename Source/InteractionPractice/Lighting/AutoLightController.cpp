6c50f09f9f34381dd1ff04088f988e6b0e0ca6c2
Sungwoo <sungwoo@example.com>
2025-11-09 12:45:07 +0900

    시간대별 자동 조명 밝기 조절 액터 구현
    - UFUNCTION(BlueprintCallable)로 블루프린트 연동
    - UPROPERTY(EditAnywhere)로 Hour 변수 에디터 노출 (0~23 범위)
    - 변수 이름 충돌 문제 해결 (InHour 사용)
    - 3인칭 템플릿 추가

---
+++ Source/InteractionPractice/Lighting/AutoLightController.cpp
@@ -0,0 +1,58 @@
+﻿
+#include "AutoLightController.h"
+#include "Components/PointLightComponent.h"
+
+
+AAutoLightController::AAutoLightController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	PointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLight"));
+	SetRootComponent(PointLight);
+
+	PointLight->AttenuationRadius = 500.f;
+	PointLight->CastShadows =  true;
+}
+
+void AAutoLightController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	int32 CurrentHour = Hour;
+
+	float Brightness = GetBrightnessByTime(CurrentHour);
+	PointLight->SetIntensity(Brightness);
+}
+
+void AAutoLightController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+float AAutoLightController::GetBrightnessByTime(int32 InHour)
+{
+	float Brightness = 0.f;
+
+	if (InHour >= 0 && InHour < 6)
+	{
+		Brightness = 200.f;
+	}
+	else if (InHour >= 6 && InHour < 12)
+	{
+		Brightness = 1500.f;
+	}
+	else if (InHour >= 12 && InHour < 18)
+	{
+		Brightness = 5000.f;
+	}
+	else
+	{
+		Brightness = 800.f;
+	}
+
+	// 현재시간과 밝기값 콘솔에 출력
+	UE_LOG(LogTemp, Warning, TEXT("현재 시간: %d시 → 조명 밝기 %.0f 루멘 적용"), InHour, Brightness);
+
+	return Brightness;
+}
+
---
+++ Source/InteractionPractice/Lighting/AutoLightController.h
@@ -0,0 +1,39 @@
+﻿
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "AutoLightController.generated.h"
+
+// 전방 선언 (헤더에 포함하는것을 최소한으로 하기 위해 사용)
+class UPointLightComponent;
+
+UCLASS()
+class INTERACTIONPRACTICE_API AAutoLightController : public AActor
+{
+	GENERATED_BODY()
+
+
+public:
+	AAutoLightController();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// 변수 선언
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPointLightComponent>	PointLight;
+
+	// 에디터에서 직접 시간을 수정할 수 있도록 설정
+	UPROPERTY(EditAnywhere, Category = "Light Control", meta = (ClampMin = "0", ClampMax = "23", AllowPrivateAccess = "true"))
+	int32 Hour = 12;
+
+	// 블루프린트 호출 가능 설정
+	UFUNCTION(BlueprintCallable)
+	float GetBrightnessByTime(int32 InHour);
+
+	
+};
