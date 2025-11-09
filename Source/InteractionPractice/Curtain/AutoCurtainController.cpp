9deeae033e4c966d430df2f71e40294ad8eea12b
Sungwoo <sungwoo@example.com>
2025-11-09 14:47:26 +0900

    feat: Auto Curtain Controller (AAutoCurtainController)
    - 풍속(WindSpeed)과 온도(Celsius)에 따라 자동으로 커튼 개방 정도 조정
    - GetCurtainOpenRatio() 함수 구현 (개방 비율 계산 및 반환)
    - UStaticMeshComponent 생성 및 회전 적용
    - BeginPlay()에서 테스트용 온도/풍속 지정 및 로그 출력

---
+++ Source/InteractionPractice/Curtain/AutoCurtainController.cpp
@@ -0,0 +1,77 @@
+﻿
+#include "AutoCurtainController.h"
+
+
+AAutoCurtainController::AAutoCurtainController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CurtainPole = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CurtainPole"));
+	SetRootComponent(CurtainPole);
+	CurtainPole->SetRelativeRotation(FRotator(0.f, 90.f, 0.f));
+
+	CurtainLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CurtainLeft"));
+	CurtainLeft->SetupAttachment(CurtainPole);
+	CurtainLeft->SetRelativeLocation(FVector(-120.f, 0.f, 0.f));
+
+	CurtainRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CurtainRight"));
+	CurtainRight->SetupAttachment(CurtainPole);
+	CurtainRight->SetRelativeLocation(FVector(120.f, 0.f, 0.f));
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> Pole(TEXT("/Script/Engine.StaticMesh'/Game/Fab/Retro_Patterned_Curtain/retro_patterned_curtain/StaticMeshes/CurtainPole.CurtainPole'"));
+	if (Pole.Succeeded())
+	{
+		CurtainPole->SetStaticMesh(Pole.Object);
+	}
+	
+	ConstructorHelpers::FObjectFinder<UStaticMesh> LeftFabric(TEXT("/Script/Engine.StaticMesh'/Game/Fab/Retro_Patterned_Curtain/retro_patterned_curtain/StaticMeshes/Curtain.Curtain'"));
+	if (LeftFabric.Succeeded())
+	{
+		CurtainLeft->SetStaticMesh(LeftFabric.Object);
+	}
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> RightFabric(TEXT("/Script/Engine.StaticMesh'/Game/Fab/Retro_Patterned_Curtain/retro_patterned_curtain/StaticMeshes/Curtain.Curtain'"));
+	if (RightFabric.Succeeded())
+	{
+		CurtainRight->SetStaticMesh(RightFabric.Object);
+	}
+	
+}
+
+void AAutoCurtainController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	float Celsius = 10.f;
+	int WindSpeed = 1;
+
+	float Ratio = GetCurtainOpenRatio(Celsius, WindSpeed);
+
+	CurtainLeft->SetRelativeRotation(FRotator(0.f, -90.f*Ratio, 0.f ));
+	CurtainRight->SetRelativeRotation(FRotator(0.f, 90.f*Ratio, 0.f));
+	
+	// 로그 찍기
+	UE_LOG(LogTemp, Warning, TEXT("현재 온도: %.1f°C, 풍속: %d m/s → 개방 비율 %.1f 적용"), Celsius, WindSpeed, Ratio);
+}
+
+void AAutoCurtainController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+float AAutoCurtainController::GetCurtainOpenRatio(float Celsius, int WindSpeed)
+{
+	// 온도 25°C 이상 또는 풍속 10m/s 이상
+	if (Celsius >= 25.f || WindSpeed >= 10)
+	{
+		return 0.2f;
+	}
+	// 온도 0~15°C, 풍속 0~5m/s
+	if ((Celsius >= 0.f && Celsius <= 15.f) && (WindSpeed >= 0 && WindSpeed <= 5))
+	{
+		return 1.f;
+	}
+	// 그 외
+	return 0.5f;
+}
+
---
+++ Source/InteractionPractice/Curtain/AutoCurtainController.h
@@ -0,0 +1,35 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AutoCurtainController.generated.h"
+
+UCLASS()
+class INTERACTIONPRACTICE_API AAutoCurtainController : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAutoCurtainController();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent>	CurtainPole;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent>	CurtainLeft;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent>	CurtainRight;
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	float GetCurtainOpenRatio(float Celsius, int WindSpeed); // 반환값 float
+};
