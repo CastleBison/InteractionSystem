c905d9dd8f73646b6ef1ce611607b9be2c6aea51
Sungwoo <sungwoo@example.com>
2025-11-10 22:09:41 +0900

    feat: 플레이어 감지형 이동 발판 구현
    - AMovingPlatform 액터 클래스 생성
    - 발판 메시 컴포넌트(PlatformPanel)를 루트로 설정하고 /Game/Mesh/PlatformPanel 메시 로드
    - 플레이어 감지용 박스 콜리전(CollisionBox) 추가 및 발판에 부착
    - BeginPlay에서 StartLocation 저장 후 Z+ 오프셋으로 TargetLocation 계산
    - Tick()에서 플레이어 오버랩 여부에 따라 StartLocation <-> TargetLocation 사이를 FMath::VInterpTo로 보간
    - OnComponentBeginOverlap / EndOverlap에 델리게이트 바인딩 후 "진입" / "이탈" 로그 출력
    
    현재 이슈:
    - 플레이어가 발판 위에 올라갔을 때 의도한 것처럼 '위로 서서히 올라가는' 느낌이 충분히 나오지 않음

---
+++ Source/InteractionPractice/Platform/MovingPlatform.cpp
@@ -0,0 +1,107 @@
+﻿
+#include "MovingPlatform.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MoveSpeed = 2.f;
+
+	// 발판 세팅
+	PlatformPanel = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlatformPanel"));
+
+	PlatformPanel->SetMobility(EComponentMobility::Movable);
+	
+	SetRootComponent(PlatformPanel);
+	PlatformPanel->SetRelativeRotation(FRotator(0.f, -180.f, 0.f));
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> Panel(TEXT("/Script/Engine.StaticMesh'/Game/Mesh/PlatformPanel.PlatformPanel'"));
+
+	if (Panel.Succeeded())
+	{
+		PlatformPanel->SetStaticMesh(Panel.Object);
+	}
+
+	// 콜리전 박스
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBox->SetupAttachment(PlatformPanel);
+
+	CollisionBox->SetBoxExtent(FVector(32.f, 35.f, 30.f));
+	CollisionBox->SetRelativeLocation(FVector(10.f, 0.f, 10.f));
+	//CollisionBox->SetRelativeScale3D(FVector(2.5f, 3.0f, 0.1f));
+	
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
+
+	float MoveOffset = 300.f;
+	TargetLocation = StartLocation + FVector(0.f, 0.f, MoveOffset);
+
+	if (CollisionBox)
+	{
+		CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AMovingPlatform::OnDetectBeginOverlap);
+		CollisionBox->OnComponentEndOverlap.AddDynamic(this, &AMovingPlatform::OnDetectEndOverlap);
+	}
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!CollisionBox)
+	{
+		return;
+	}
+
+	ACharacter* Player = UGameplayStatics::GetPlayerCharacter(this, 0);
+
+	FVector CurrentLocation = GetActorLocation();
+	FVector Target = StartLocation;
+
+	if (Player && CollisionBox->IsOverlappingActor(Player))
+	{
+		Target = TargetLocation;
+	}
+
+	FVector NewLocation = FMath::VInterpTo(CurrentLocation, Target, DeltaTime, MoveSpeed);
+
+	SetActorLocation(NewLocation);
+}
+
+void AMovingPlatform::OnDetectBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor || OtherActor == this)
+	{
+		return;
+	}
+
+	if (Cast<ACharacter>(OtherActor))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("진입"));
+	}
+}
+
+void AMovingPlatform::OnDetectEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (!OtherActor || OtherActor == this)
+	{
+		return;
+	}
+
+	if (Cast<ACharacter>(OtherActor))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("이탈"));
+	}
+}
+
---
+++ Source/InteractionPractice/Platform/MovingPlatform.h
@@ -0,0 +1,53 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+// 전방 선언
+class UBoxComponent;
+
+UCLASS()
+class INTERACTIONPRACTICE_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMovingPlatform();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// 발판 메시
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent>	PlatformPanel;
+
+	// 박스 콜리전
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UBoxComponent>			CollisionBox;
+
+	// 시작 위치
+	FVector StartLocation;
+
+	// 목표 위치
+	FVector TargetLocation;
+
+	// 이동 속도
+	UPROPERTY(EditAnywhere)
+	float MoveSpeed = 2.f;
+
+	// 오버랩 시작
+	UFUNCTION()
+	void OnDetectBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// 오버랩 종료
+	UFUNCTION()
+	void OnDetectEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
