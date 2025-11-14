0188db085dbc0d1f10380525fc3a76bd524c3f60
Sungwoo <castle.ren@pluxity.com>
2025-11-14 16:22:14 +0900

    오버랩 이벤트 추가 및 층별 위치 배열 구성

--- Source/InteractionPractice/Elevator/Elevator.cpp
+++ Source/InteractionPractice/Elevator/Elevator.cpp
@@ -1,20 +1,81 @@
 
 #include "Elevator.h"
 
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+
+
 
 AElevator::AElevator()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	ElevatorFrame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ElevatorFrame"));
+	SetRootComponent(ElevatorFrame);
+	ElevatorFrame->SetRelativeRotation(FRotator(0.f, 90.f, 0.f));
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> Frame(TEXT("/Script/Engine.StaticMesh'/Game/Mesh/SM_ElevatorBody.SM_ElevatorBody'"));
+	if (Frame.Succeeded())
+	{
+		ElevatorFrame->SetStaticMesh(Frame.Object);
+	}
+	
+	ElevatorBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ElevatorBody"));
+	ElevatorBody->SetupAttachment(ElevatorFrame);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> Body(TEXT("/Script/Engine.StaticMesh'/Game/Mesh/ElecvatorInner.ElecvatorInner'"));
+	if (Body.Succeeded())
+	{
+		ElevatorBody->SetStaticMesh(Body.Object);
+	}
+
+	OverlapBox = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapBox"));
+	OverlapBox->SetupAttachment(ElevatorFrame);
+	OverlapBox->SetBoxExtent(FVector(60.f, 60.f, 10.f));
+	OverlapBox->SetRelativeLocation(FVector(0.f, 0.f, 3.f));
+	
+	OverlapBox->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlapEvent);
+
+	// 층별 위치값 배열 
+	FloorLocationArray.Add(0.f);
+	FloorLocationArray.Add(260.f);
+	FloorLocationArray.Add(520.f);
+	FloorLocationArray.Add(780.f);
+	FloorLocationArray.Add(1040.f);
 }
 
 void AElevator::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void AElevator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AElevator::OnOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,	const FHitResult & SweepResult)
+{
+	if (OtherActor->ActorHasTag("Player"))
+	{
+		if (!bIsActive)
+		{
+			// 오버랩이 시작되면, 지정된 목표층(GoalFloor) 인덱스를 이용해 배열에서 해당 층의 위치를 가져온 뒤 그 위치로 엘리베이터를 이동
+			bIsActive = true;
+			UE_LOG(LogTemp, Warning, TEXT("플레이어 진입")); // 오버랩 시작
+
+			if (GoalFloor > FloorLocationArray.Num()) return; // 예외처리
+		
+			
+			float SelectFloorLocation = FloorLocationArray[GoalFloor]; // 인덱스를 이용해서 배열에 접근 후 출력
+			
+			UE_LOG(LogTemp,Warning,TEXT("select floor location : %f"), SelectFloorLocation);
+		}
+	}
+}
+
+void AElevator::UpMovement()
+{
+	
 }
 
--- Source/InteractionPractice/Elevator/Elevator.h
+++ Source/InteractionPractice/Elevator/Elevator.h
@@ -1,9 +1,11 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Elevator.generated.h"
+
+// 전방선언
+class UBoxComponent;
 
 UCLASS()
 class INTERACTIONPRACTICE_API AElevator : public AActor
@@ -18,4 +20,33 @@ protected:
 
 public:
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent>	ElevatorFrame;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent>	ElevatorBody;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UBoxComponent>			OverlapBox;
+
+	bool bIsActive = false;
+
+protected:
+	UFUNCTION()
+	void OnOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,	bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable)
+	void UpMovement();
+
+	// 층 배열
+	UPROPERTY(EditAnywhere)
+	TArray<int32>	FloorLocationArray;
+
+	// 에디터에서 목표층을 설정할 수 있는 변수 선언
+	UPROPERTY(EditAnywhere)
+	int32 GoalFloor = 0;
+
+	int32 MaxFloor = 100;
 };
